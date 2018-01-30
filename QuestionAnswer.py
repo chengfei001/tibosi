5558e55e59e75e5cc54d52d93aa0018fc3747a8a
chengfei <chengfei001@gmail.com>
2018-01-30 17:07:15 +0800

    修改多选题哪个是正确答案的判断

--- QuestionAnswer.py
+++ QuestionAnswer.py
@@ -14,11 +14,10 @@ db_questionItem = db.QuestionItem
 
 class QuestionAnswer:
     def __init__(self):
-        # self.appENames = "['ZYYS_EK']"
+        # self.appENames = ["ZYYS_KQHMWK"]
         self.appENames = ["ZYYS_KQHMWK","ZYYS_FSK","ZYYS_ZYT","ZYYS_ZYEBYHK","ZYYS_KFYX","ZYYS_PFK","ZYYS_QKYX","ZYYS_SJNK","ZYYS_YK","ZYYS_YXYX","ZYYS_GK","ZYYS_HYXK","ZYYS_EK","ZYYS_FCK","ZYYS_NK","ZYYS_WK","ZYYS_EWK","ZYYS_JSK","ZYYS_JZK","ZYYS_MZK","ZYYS_EBYHK","ZYYS_EXJYK","ZYYS_LCBLK","ZYYS_CSYXK","ZYYS_MNWK","ZYYS_XXWK","ZYYS_ZXWK","ZYYS_FSZLK","ZYYS_KQBLK","ZYYS_KQNK","ZYYS_KQQK","ZYYS_KQXFK","ZYYS_KQZJK","ZYYS_TNK","ZYYS_YFYXK","ZYYS_ZJK","ZYYS_ZYEK","ZYYS_ZYFK","ZYYS_ZYGSK","ZYYS_ZYKFK","ZYYS_ZYNK","ZYYS_ZYQK","ZYYS_ZYWK","ZYYS_ZYYK","ZYYS_JSNKYJD"]
 
     def get_question_answer(self):
-
         logging.info(self.appENames)
         for question_item in db_questionItem.find({'appEName':{'$in': self.appENames}}):
             # logging.info(question_item)
@@ -36,6 +35,26 @@ class QuestionAnswer:
                         question_answer['Content'] = question_item['Answer']
                         db_questionAnswer.insert(question_answer.copy())
             # ATEST BTEST A3
+            elif question_item['Type'] == 'XTEST':
+                if question_item.get('SelectedItems') is not None:
+                    for answer_item  in question_item['SelectedItems']:
+                        if answer_item.get('HasImg'):
+                            answer_item['HasImg'] = answer_item['HasImg']
+                        else:
+                            answer_item['HasImg'] = 0
+
+                        question_answer['ItemName'] = answer_item['ItemName']
+                        question_answer['Content'] = answer_item['Content']
+                        i = 0
+                        while i < len(question_item['Answer']):
+                            if answer_item['ItemName'] == question_item['Answer'][i]:
+                                IsRight = 1
+                                break
+                            else:
+                                IsRight = 0
+                            i += 1
+                        question_answer['IsRight'] = IsRight
+                        db_questionAnswer.insert(question_answer.copy())
             else:
                 if question_item.get('SelectedItems') is not None:
                     for answer_item  in question_item['SelectedItems']:
